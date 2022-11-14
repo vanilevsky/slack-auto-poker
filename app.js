91225b3772fdf62d26033f9f2c4e1be94278c5f7
vanilevsky <dev@skij.ru>
2022-11-14 17:11:35 +0800

    Add connect_with_poker step

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const { App } = require('@slack/bolt');
+const { App, WorkflowStep } = require('@slack/bolt');
+const fetch = require("node-fetch");
 
-/* 
+/*
 This sample slack application uses SocketMode
-For the companion getting started setup guide, 
-see: https://slack.dev/bolt-js/tutorial/getting-started 
+For the companion getting started setup guide,
+see: https://slack.dev/bolt-js/tutorial/getting-started
 */
 
 // Initializes your app with your bot token and app token
@@ -43,6 +44,139 @@ app.action('button_click', async ({ body, ack, say }) => {
   await ack();
   await say(`<@${body.user.id}> clicked the button`);
 });
+
+const ws = new WorkflowStep('connect_with_poker', {
+  edit: async ({ ack, step, configure }) => {
+    await ack();
+
+    const blocks = [
+      {
+        type: 'input',
+        block_id: 'task_name_input',
+        element: {
+          type: 'plain_text_input',
+          action_id: 'name',
+          placeholder: {
+            type: 'plain_text',
+            text: 'Add a task name',
+          },
+        },
+        label: {
+          type: 'plain_text',
+          text: 'Task name',
+        },
+      },
+      {
+        type: 'input',
+        block_id: 'task_description_input',
+        element: {
+          type: 'plain_text_input',
+          action_id: 'description',
+          placeholder: {
+            type: 'plain_text',
+            text: 'Add a task description',
+          },
+        },
+        label: {
+          type: 'plain_text',
+          text: 'Task description',
+        },
+      },
+    ];
+
+    await configure({ blocks });
+  },
+  save: async ({ ack, step, view, update }) => {
+    await ack();
+
+    const { values } = view.state;
+    const taskName = values.task_name_input.name;
+    const taskDescription = values.task_description_input.description;
+
+    const inputs = {
+      taskName: { value: taskName.value },
+      taskDescription: { value: taskDescription.value }
+    };
+
+    const outputs = [
+      {
+        type: 'text',
+        name: 'taskName',
+        label: 'Task name',
+      },
+      {
+        type: 'text',
+        name: 'taskDescription',
+        label: 'Task description',
+      }
+    ];
+
+    await update({ inputs, outputs });
+  },
+  execute: async ({ step, complete, fail }) => {
+
+    const { inputs } = step;
+
+    const outputs = {
+      taskName: inputs.taskName.value,
+      taskDescription: inputs.taskDescription.value,
+    };
+
+
+
+
+
+
+    const myHeaders = new fetch.Headers();
+    myHeaders.append("Shortcut-Token", "");
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "external_links": [
+        ""
+      ]
+    });
+
+    const requestOptions = {
+      method: 'PUT',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch("", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+
+
+
+
+
+
+
+
+    // signal back to Slack that everything was successful
+    // await complete({ outputs });
+    // NOTE: If you run your app with processBeforeResponse: true option,
+    // `await complete()` is not recommended because of the slow response of the API endpoint
+    // which could result in not responding to the Slack Events API within the required 3 seconds
+    // instead, use:
+    complete({outputs}).then(() => {
+      console.log('workflow step execution complete registered');
+    });
+
+    // let Slack know if something went wrong
+    // await fail({ error: { message: "Just testing step failure!" } });
+    // NOTE: If you run your app with processBeforeResponse: true, use this instead:
+    fail({error: {message: "Just testing step failure!"}}).then(() => {
+      console.log('workflow step execution failure registered');
+    });
+  },
+});
+
+app.step(ws);
 
 (async () => {
   // Start your app
