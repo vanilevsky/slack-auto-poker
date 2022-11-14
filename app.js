66d3b673753ab3c11c8b47549053ef5148d036e9
vanilevsky <dev@skij.ru>
2022-11-14 18:04:32 +0800

    Remove hello and button click

--- app.js
+++ app.js
@@ -7,36 +7,6 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN
 });
 
-app.message('hello', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say({
-    blocks: [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
-          },
-          "action_id": "button_click"
-        }
-      }
-    ],
-    text: `Hey there <@${message.user}>!`
-  });
-});
-
-app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
-});
-
 const ws = new WorkflowStep('connect_with_poker', {
   edit: async ({ ack, step, configure }) => {
     await ack();
