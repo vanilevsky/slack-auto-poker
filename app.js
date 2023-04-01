4051ced1aadcc9b13ae85f0318cbdd98ff6ac016
vanilevsky <vanilevsky@gmail.com>
2023-04-01 19:17:51 +0800

    Make code more readable

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const { App, WorkflowStep } = require('@slack/bolt');
-const fetch = require("node-fetch");
-const express = require("express");
-require('dotenv').config();
+const fetch = require('node-fetch');
+const express = require('express');
+const dotenv = require('dotenv');
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN
+dotenv.config();
+
+const app = createSlackApp();
+const server = createWebhookServer();
+
+server.listen(process.env.WEBHOOK_PORT || 8000, () => {
+  console.log(`⚡️ Webhook listener is running on port ${process.env.WEBHOOK_PORT || 8000}`);
 });
 
-// Add Express for webhook handling
-const server = express();
-server.use(express.json());
+app.step(createWorkflowStep());
 
-// Webhook listener
-server.post('/webhook', async (req, res) => {
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
+})();
+
+function createSlackApp() {
+  return new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN
+  });
+}
+
+function createWebhookServer() {
+  const server = express();
+  server.use(express.json());
+  server.post('/webhook', webhookHandler);
+  return server;
+}
+
+async function webhookHandler(req, res) {
   const data = req.body;
   const action = data.actions && data.actions[0] ? data.actions[0] : null;
   const estimateLabelIds = [
@@ -52,134 +72,125 @@ server.post('/webhook', async (req, res) => {
   }
 
   res.sendStatus(200);
-});
+}
 
-// Start Express server for webhook handling
-server.listen(process.env.WEBHOOK_PORT || 8000, () => {
-  console.log(`⚡️ Webhook listener is running on port ${process.env.WEBHOOK_PORT || 8000}`);
-});
+function createWorkflowStep() {
+  return new WorkflowStep('connect_with_poker', {
+    edit,
+    save,
+    execute
+  });
+}
 
-// ... (the rest of your app.js code)
+async function edit({ ack, step, configure }) {
+  await ack();
 
-const ws = new WorkflowStep('connect_with_poker', {
-  edit: async ({ ack, step, configure }) => {
-    await ack();
-
-    const blocks = [
-      {
-        type: 'input',
-        block_id: 'shortcut_story_id_input',
-        element: {
-          type: 'plain_text_input',
-          action_id: 'story_id',
-          placeholder: {
-            type: 'plain_text',
-            text: 'Add a Shortcut story ID',
-          },
-        },
-        label: {
+  const blocks = [
+    {
+      type: 'input',
+      block_id: 'shortcut_story_id_input',
+      element: {
+        type: 'plain_text_input',
+        action_id: 'story_id',
+        placeholder: {
           type: 'plain_text',
-          text: 'Story ID',
+          text: 'Add a Shortcut story ID',
         },
       },
-      {
-        type: 'input',
-        block_id: 'slack_published_poker_link_input',
-        element: {
-          type: 'plain_text_input',
-          action_id: 'poker_link',
-          placeholder: {
-            type: 'plain_text',
-            text: 'Add a Slack link to published poker',
-          },
-        },
-        label: {
+      label: {
+        type: 'plain_text',
+        text: 'Story ID',
+      },
+    },
+    {
+      type: 'input',
+      block_id: 'slack_published_poker_link_input',
+      element: {
+        type: 'plain_text_input',
+        action_id: 'poker_link',
+        placeholder: {
           type: 'plain_text',
-          text: 'Poker link',
+          text: 'Add a Slack link to published poker',
         },
       },
-    ];
-
-    await configure({ blocks });
-  },
-  save: async ({ ack, step, view, update }) => {
-    await ack();
-
-    const {values} = view.state;
-    const storyId = values.shortcut_story_id_input.story_id.value;
-    const pokerLink = values.slack_published_poker_link_input.poker_link.value;
-
-    const inputs = {
-      storyId: {value: storyId},
-      pokerLink: {value: pokerLink},
-    };
-
-    const outputs = [
-      {
-        type: 'text',
-        name: 'storyId',
-        label: 'Story ID',
+      label: {
+        type: 'plain_text',
+        text: 'Poker link',
       },
-      {
-        type: 'text',
-        name: 'pokerLink',
-        label: 'Poker link',
-      }
-    ];
+    },
+  ];
 
-    await update({inputs, outputs});
-  },
-  execute: async ({ step, complete, fail }) => {
+  await configure({ blocks });
+}
 
-    const { inputs } = step;
+async function save({ ack, step, view, update }) {
+  await ack();
 
-    const outputs = {
-      shortcutCardId: inputs.storyId.value,
-      cardExternalLinks: [
-          inputs.pokerLink.value + '?poker',
-      ]
-    };
+  const {values} = view.state;
+  const storyId = values.shortcut_story_id_input.story_id.value;
+  const pokerLink = values.slack_published_poker_link_input.poker_link.value;
 
-    updateShortcutCardExternalLinks(outputs.shortcutCardId, outputs.cardExternalLinks)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+  const inputs = {
+    storyId: {value: storyId},
+    pokerLink: {value: pokerLink},
+  };
+
+  const outputs = [
+    {
+      type: 'text',
+      name: 'storyId',
+      label: 'Story ID',
+    },
+    {
+      type: 'text',
+      name: 'pokerLink',
+      label: 'Poker link',
+    }
+  ];
+
+  await update({inputs, outputs});
+}
+
+async function execute({ step, complete, fail }) {
+
+  const { inputs } = step;
+
+  const outputs = {
+    shortcutCardId: inputs.storyId.value,
+    cardExternalLinks: [
+      inputs.pokerLink.value + '?poker',
+    ]
+  };
+
+  updateShortcutCardExternalLinks(outputs.shortcutCardId, outputs.cardExternalLinks)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
 
 
 
 
 
 
-    // signal back to Slack that everything was successful
-    // await complete({ outputs });
-    // NOTE: If you run your app with processBeforeResponse: true option,
-    // `await complete()` is not recommended because of the slow response of the API endpoint
-    // which could result in not responding to the Slack Events API within the required 3 seconds
-    // instead, use:
-    complete({outputs}).then(() => {
-      console.log('workflow step execution complete registered');
-    });
+  // signal back to Slack that everything was successful
+  // await complete({ outputs });
+  // NOTE: If you run your app with processBeforeResponse: true option,
+  // `await complete()` is not recommended because of the slow response of the API endpoint
+  // which could result in not responding to the Slack Events API within the required 3 seconds
+  // instead, use:
+  complete({outputs}).then(() => {
+    console.log('workflow step execution complete registered');
+  });
 
-    // let Slack know if something went wrong
-    // await fail({ error: { message: "Just testing step failure!" } });
-    // NOTE: If you run your app with processBeforeResponse: true, use this instead:
-    fail({error: {message: "Just testing step failure!"}}).then(() => {
-      console.log('workflow step execution failure registered');
-    });
-  },
-});
+  // let Slack know if something went wrong
+  // await fail({ error: { message: "Just testing step failure!" } });
+  // NOTE: If you run your app with processBeforeResponse: true, use this instead:
+  fail({error: {message: "Just testing step failure!"}}).then(() => {
+    console.log('workflow step execution failure registered');
+  });
+}
 
-app.step(ws);
-
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
-})();
-
-async function updateShortcutCardExternalLinks (cardId, externalLinks) {
-
+async function updateShortcutCardExternalLinks(cardId, externalLinks) {
   const headers = new fetch.Headers();
   headers.append("Shortcut-Token", process.env.SHORTCUT_TOKEN);
   headers.append("Content-Type", "application/json");
