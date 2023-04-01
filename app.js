a8cc64123401f3aeb927635f2a5a1c9a0b540174
vanilevsky <vanilevsky@gmail.com>
2023-04-01 13:05:04 +0800

    Include php to js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { App, WorkflowStep } = require('@slack/bolt');
 const fetch = require("node-fetch");
+const express = require("express");
 require('dotenv').config();
 
 const app = new App({
@@ -7,6 +8,58 @@ const app = new App({
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN
 });
+
+// Add Express for webhook handling
+const server = express();
+server.use(express.json());
+
+// Webhook listener
+server.post('/webhook', async (req, res) => {
+  const data = req.body;
+  const action = data.actions && data.actions[0] ? data.actions[0] : null;
+  const estimateLabelIds = [
+    15989, // "poker"
+    24385, // "poker-2"
+  ];
+  const slackWebhook = process.env.SLACK_WEBHOOK_URL;
+
+  if (action &&
+      action.id &&
+      action.name &&
+      action.app_url &&
+      action.entity_type === 'story' &&
+      action.action === 'update' &&
+      action.changes &&
+      action.changes.label_ids &&
+      action.changes.label_ids.adds &&
+      action.changes.label_ids.adds.some(item => estimateLabelIds.includes(item))
+  ) {
+
+    const webhookData = {
+      "shortcut_story_name": action.name,
+      "shortcut_story_url": action.app_url,
+      "shortcut_story_id": String(action.id)
+    };
+
+    const response = await fetch(slackWebhook, {
+      method: 'POST',
+      body: JSON.stringify(webhookData),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const result = await response.text();
+    console.log(result);
+  }
+
+  res.sendStatus(200);
+});
+
+// Start Express server for webhook handling
+server.listen(process.env.WEBHOOK_PORT || 8000, () => {
+  console.log(`⚡️ Webhook listener is running on port ${process.env.WEBHOOK_PORT || 8000}`);
+});
+
+// ... (the rest of your app.js code)
 
 const ws = new WorkflowStep('connect_with_poker', {
   edit: async ({ ack, step, configure }) => {
